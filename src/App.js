50fb408c87559caa71bdb57acda59abaebb81a00
Abhay Shekhawat <shekhawat.abhay94@gmail.com>
2020-08-21 14:53:04 +0530

    added header component and relevent links

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch , Route } from 'react-router-dom';
 import './App.css';
 import './pages/homepage/homepage.component';
 import HomePage from './pages/homepage/homepage.component';
+import Header from './components/header/header.component';
 import ShopPage from './pages/shop/shop.component';
 
 
@@ -12,6 +13,7 @@ function App() {
     <div>
       {/* Switch - The moment it finds a mtached url 
       - it won't look further - if we remove exact,it will just match the first component that matches the route */}
+      <Header/>
       <Switch>
         <Route exact  path = '/' component = {HomePage} />
         <Route exact path = '/shop' component = {ShopPage} />
