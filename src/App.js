d78c0c955130b3c5c1edfe9ede9bf731100e1073
Abhay Shekhawat <shekhawat.abhay94@gmail.com>
2020-09-01 23:32:45 +0530

    added redux and implemented user reducer and actions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch , Route } from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import './App.css';
 import './pages/homepage/homepage.component';
@@ -8,38 +9,32 @@ import Header from './components/header/header.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import {auth,createUserProfileDocument} from './firebase/firebase.utils';
+import {setCurrentUser} from './redux/user/user.actions';
+
 
  {/* Switch - The moment it finds a mtached url 
       - it won't look further - if we remove exact,it will just match the first component that matches the route */}
       
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null;
 
   componentDidMount(){
+    const {setCurrentUser} = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth =>{
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
         //console.log(userRef);
         userRef.onSnapshot(snapshot =>{
-          this.setState({
-            currentUser:{
-              id: snapshot.id,
-              ...snapshot.data()
-            }
+          setCurrentUser({
+            id: snapshot.id,
+            ...snapshot.data()
           });
         });
       }
       
       else{
-        this.setState({currentUser:userAuth})
+        setCurrentUser(userAuth);
       }
     });
   }
@@ -51,7 +46,7 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        <Header currentUser ={this.state.currentUser}/>
+        <Header/>
         <Switch>
           <Route exact  path = '/' component = {HomePage} />
           <Route exact path = '/shop' component = {ShopPage} />
@@ -62,4 +57,9 @@ class App extends React.Component {
   }
 }
 
-export default App;
+//invoking the setCurrentUser action with the user payload- which returns the objecct - so we are just dispatching the object
+const mapDispatchToProps = dispatch =>({
+  setCurrentUser : user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null,mapDispatchToProps)(App);
