e0fc9896efd7047927ccce20827e84701b8a8e50
Abhay Shekhawat <shekhawat.abhay94@gmail.com>
2020-09-04 21:25:42 +0530

    updated shop data and updated cart utils to group cart items

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,3 +4,8 @@ import CartAActionTypes from './cart.types';
 export const toggleCartHidden = () =>({
     type:CartAActionTypes.TOGGLE_CART_HIDDEN
 });
+
+export const addItem = item =>({
+    type:CartAActionTypes.ADD_ITEM,
+    payload:item
+});
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,10 @@
+export const addItemToCart = (cartItems, cartItemToAdd) =>{
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
+    if(existingCartItem){
+        return cartItems.map(cartItem => cartItem.Id === cartItemToAdd.id
+            ?{...cartItem,quantity:cartItem.quantity+1}:
+            cartItem)
+    }
+
+    return [...cartItems,{...cartItemToAdd,quantity:1}];
+}
